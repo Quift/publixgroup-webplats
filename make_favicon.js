f40b48606aa38299d8f6d3390a26f79903cadb30
Pierre <pierreemil@gmail.com>
2026-07-03 15:12:55 +0200

    Fix Harri image, align header CTA with hero, update DE bio

---
+++ make_favicon.js
@@ -0,0 +1,3 @@
+﻿const sharp = require("sharp");
+sharp("site/favicon.svg").resize(32, 32).png().toFile("site/favicon.png");
+sharp("site/favicon.svg").resize(180, 180).png().toFile("site/apple-touch-icon.png");
